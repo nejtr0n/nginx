e1594bb14477b0a63d89ad74c4fd0139eb2d5ec6
nejtr0n <a6y@xakep.ru>
2016-05-10 16:39:01 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-consul-template -consul=$CONSUL_URL -wait "10s" -retry 10s -template="/templates/$NGINX_TEMPLATE:/etc/nginx/conf.d/default.conf:service nginx reload" &
-sleep 15
-nginx -g 'daemon off;'
+tail -f /dev/null
+#consul-template -consul=$CONSUL_URL -wait "10s" -retry 10s -template="/templates/$NGINX_TEMPLATE:/etc/nginx/conf.d/default.conf:service nginx reload" &
+#sleep 15
+#nginx -g 'daemon off;'
